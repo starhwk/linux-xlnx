8154a7b3885f8b303d4ebfbb7b7e109e246a706d
Hyun Kwon <hyun.kwon@xilinx.com>
2018-02-27 18:07:47 -0800

    drm: xlnx: bridge: Remove duplicate license paragraphs
    Remove the license paragraphs that are simply duplicates
    of SPDX identifier.
    
    Signed-off-by: Hyun Kwon <hyun.kwon@xilinx.com>

--- drivers/gpu/drm/xlnx/xlnx_bridge.c
+++ drivers/gpu/drm/xlnx/xlnx_bridge.c
@@ -5,15 +5,6 @@
  *  Copyright (C) 2017 Xilinx, Inc.
  *
  *  Author: Hyun Woo Kwon <hyun.kwon@xilinx.com>
- *
- * This software is licensed under the terms of the GNU General Public
- * License version 2, as published by the Free Software Foundation, and
- * may be copied, distributed, and modified under those terms.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
  */
 
 #include <drm/drmP.h>
